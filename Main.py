a9e2abc6e30839bd3f5b1db8a2cc9b5245f283fe
TLio2407 <phandinhcuong02@gmail.com>
2025-07-21 21:51:58 +0700

    21072025 Basic Solution

--- Main.py
+++ Main.py
@@ -1,6 +1,5 @@
 import InputData as data
 import NorthWestCorner as nwc
-import LeastCostCell as lcc
 import Optimize as opt
 demand_number = int(input("Input number of demands: "))
 supply_number = int(input("Input number of supplies: "))
@@ -9,7 +8,6 @@ demand_arr, supply_arr = data.inputDemandandSupply(demand_number,supply_number)
 cost_matrix = data.inputCostmatrix(demand_number,supply_number)
 data.printData(demand_arr, supply_arr, cost_matrix)
 nwc_matrix = nwc.NWC(demand_arr, supply_arr, cost_matrix)
-lcc_matrix = lcc.LCC(demand_arr, supply_arr, cost_matrix)
 result_matrix = opt.optimize(nwc_matrix,cost_matrix)
 print("FINAL MATRIX:")
 for _ in result_matrix:
